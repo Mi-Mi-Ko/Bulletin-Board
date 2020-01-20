835a167db7591edda770d82c5bb91817b8368204
mimiko 117 <scm.mimiko@gmail.com>
2020-01-20 13:44:06 +0630

    prevent back history

--- routes/web.php
+++ routes/web.php
@@ -2,104 +2,106 @@
 
 use Illuminate\Support\Facades\Route;
 
-/**
- * Login Routes
- */
-Route::get('/', 'Auth\LoginController@showLogin')
-    ->name('showLogin');
-
-Route::group(['prefix' => 'login'], function () {
+Route::group(['middleware' => 'prevent-back-history'], function () {
+    /**
+     * Login Routes
+     */
     Route::get('/', 'Auth\LoginController@showLogin')
         ->name('showLogin');
-    Route::post('/', 'Auth\LoginController@login')
-        ->name('login');
-});
 
-/**
- * User Routes Group
- * Allow For Admin
- */
-Route::group(['middleware' => 'admin'], function () {
-    Route::group(['prefix' => 'users'], function () {
-        Route::get('/create', 'User\UserController@create')
-            ->name('users#create');
-        Route::post('/confirm', 'User\UserController@confirmation')
-            ->name('users#confirmation');
-        Route::get('/backInput', 'User\UserController@backUserInput')
-            ->name('users#backUserInput');
-        Route::post('/', 'User\UserController@store')
-            ->name('users#store');
+    Route::group(['prefix' => 'login'], function () {
+        Route::get('/', 'Auth\LoginController@showLogin')
+            ->name('showLogin');
+        Route::post('/', 'Auth\LoginController@login')
+            ->name('login');
     });
-});
 
-/**
- * User Profile Routes
- * Allow For User
- */
-Route::group(['middleware' => 'user'], function () {
-    Route::group(['prefix' => 'users'], function () {
-        Route::get('/profile/{id}', 'User\UserController@profile')
-            ->name('users#profile');
+    /**
+     * User Routes Group
+     * Allow For Admin
+     */
+    Route::group(['middleware' => 'admin'], function () {
+        Route::group(['prefix' => 'users'], function () {
+            Route::get('/create', 'User\UserController@create')
+                ->name('users#create');
+            Route::post('/confirm', 'User\UserController@confirmation')
+                ->name('users#confirmation');
+            Route::get('/backInput', 'User\UserController@backUserInput')
+                ->name('users#backUserInput');
+            Route::post('/', 'User\UserController@store')
+                ->name('users#store');
+        });
     });
-});
 
-/**
- * User And Post Routes Group
- * Allow For Login User
- */
-Route::group(['middleware' => 'login'], function () {
-    Route::group(['prefix' => 'users'], function () {
-        Route::get('/', 'User\UserController@index')
-            ->name('users#index');
-        Route::get('/backUpdate', 'User\UserController@backUserUpdate')
-            ->name('users#backUserUpdate');
-        Route::any('/search', 'User\UserController@search')
-            ->name('users#search');
-        Route::get('/{id}', 'User\UserController@show')
-            ->name('users#show');
-        Route::any('/{id}/updateConfirmation', 'User\UserController@updateConfirmation')
-            ->name('users#updateConfirmation');
-        Route::post('/{id}', 'User\UserController@update')
-            ->name('users#update');
-        Route::delete('/{id}', 'User\UserController@delete')
-            ->name('users#delete');
+    /**
+     * User Profile Routes
+     * Allow For User
+     */
+    Route::group(['middleware' => 'user'], function () {
+        Route::group(['prefix' => 'users'], function () {
+            Route::get('/profile/{id}', 'User\UserController@profile')
+                ->name('users#profile');
+        });
     });
-    Route::group(['prefix' => 'posts'], function () {
-        Route::get('/', 'Post\PostController@index')
-            ->name('posts#index');
-        Route::any('/search', 'Post\PostController@search')
-            ->name('posts#search');
-        Route::get('/create', 'Post\PostController@create')
-            ->name('posts#create');
-        Route::post('/confirm', 'Post\PostController@confirmation')
-            ->name('posts#confirmation');
-        Route::get('/backInput', 'Post\PostController@backPostInput')
-            ->name('posts#backPostInput');
-        Route::get('/backUpdate', 'Post\PostController@backPostUpdate')
-            ->name('posts#backPostUpdate');
-        Route::post('/', 'Post\PostController@store')
-            ->name('posts#store');
-        Route::get('/importView', 'Post\PostController@getCsv')
-            ->name('posts#getCsv');
-        Route::post('/import', 'Post\PostController@import')
-            ->name('posts#import');
-        Route::get('/export', 'Post\PostController@export')
-            ->name('posts#export');
-        Route::get('/{id}', 'Post\PostController@show')
-            ->name('posts#show');
-        Route::any('/{id}/updateConfirmation', 'Post\PostController@updateConfirmation')
-            ->name('posts#updateConfirmation');
-        Route::put('/{post}', 'Post\PostController@update')
-            ->name('posts#update');
-        Route::delete('/{id}', 'Post\PostController@delete')
-            ->name('posts#delete');
+
+    /**
+     * User And Post Routes Group
+     * Allow For Login User
+     */
+    Route::group(['middleware' => 'login'], function () {
+        Route::group(['prefix' => 'users'], function () {
+            Route::get('/', 'User\UserController@index')
+                ->name('users#index');
+            Route::get('/backUpdate', 'User\UserController@backUserUpdate')
+                ->name('users#backUserUpdate');
+            Route::any('/search', 'User\UserController@search')
+                ->name('users#search');
+            Route::get('/{id}', 'User\UserController@show')
+                ->name('users#show');
+            Route::any('/{id}/updateConfirmation', 'User\UserController@updateConfirmation')
+                ->name('users#updateConfirmation');
+            Route::post('/{id}', 'User\UserController@update')
+                ->name('users#update');
+            Route::delete('/{id}', 'User\UserController@delete')
+                ->name('users#delete');
+        });
+        Route::group(['prefix' => 'posts'], function () {
+            Route::get('/', 'Post\PostController@index')
+                ->name('posts#index');
+            Route::any('/search', 'Post\PostController@search')
+                ->name('posts#search');
+            Route::get('/create', 'Post\PostController@create')
+                ->name('posts#create');
+            Route::post('/confirm', 'Post\PostController@confirmation')
+                ->name('posts#confirmation');
+            Route::get('/backInput', 'Post\PostController@backPostInput')
+                ->name('posts#backPostInput');
+            Route::get('/backUpdate', 'Post\PostController@backPostUpdate')
+                ->name('posts#backPostUpdate');
+            Route::post('/', 'Post\PostController@store')
+                ->name('posts#store');
+            Route::get('/importView', 'Post\PostController@getCsv')
+                ->name('posts#getCsv');
+            Route::post('/import', 'Post\PostController@import')
+                ->name('posts#import');
+            Route::get('/export', 'Post\PostController@export')
+                ->name('posts#export');
+            Route::get('/{id}', 'Post\PostController@show')
+                ->name('posts#show');
+            Route::any('/{id}/updateConfirmation', 'Post\PostController@updateConfirmation')
+                ->name('posts#updateConfirmation');
+            Route::put('/{post}', 'Post\PostController@update')
+                ->name('posts#update');
+            Route::delete('/{id}', 'Post\PostController@delete')
+                ->name('posts#delete');
+        });
+        Route::group(['prefix' => 'password'], function () {
+            Route::get('/change/{id}', 'Auth\ResetPasswordController@showChangePasswordForm')
+                ->name('password#showChangePasswordForm');
+            Route::post('/change', 'Auth\ResetPasswordController@change')
+                ->name('password#change');
+        });
+        Route::post('/logout', 'Auth\LoginController@logout')
+            ->name('logout');
     });
-    Route::group(['prefix' => 'password'], function () {
-        Route::get('/change/{id}', 'Auth\ResetPasswordController@showChangePasswordForm')
-            ->name('password#showChangePasswordForm');
-        Route::post('/change', 'Auth\ResetPasswordController@change')
-            ->name('password#change');
-    });
-    Route::post('/logout', 'Auth\LoginController@logout')
-        ->name('logout');
 });
